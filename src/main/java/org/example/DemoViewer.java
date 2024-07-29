6e2cda6800306ec1866e2b0968ce61629e2ebbaf
Gigio42 <gigio.42@gmail.com>
2024-07-29 16:34:49 -0300

    Implementação de sombreamento com luz direcional e calculo de normal com incidencia de luz

--- src/main/java/org/example/DemoViewer.java
+++ src/main/java/org/example/DemoViewer.java
@@ -76,31 +76,47 @@ public class DemoViewer {
                     v3.x += getWidth() / 2;
                     v3.y += getHeight() / 2;
 
+                    // Define vectors ab and ac
+                    Vertex ab = new Vertex(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
+                    Vertex ac = new Vertex(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
+
+                    // calculate normal vector
+                    Vertex norm = new Vertex(
+                            ab.y * ac.z - ab.z * ac.y,
+                            ab.z * ac.x - ab.x * ac.z,
+                            ab.x * ac.y - ab.y * ac.x
+                    );
+                    double normalLength = Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
+                    norm.x /= normalLength;
+                    norm.y /= normalLength;
+                    norm.z /= normalLength;
+
+                    // compute dot product of light direction and normal vector
+                    double angleCos = Math.abs(norm.z);
+
                     // compute rectangular bounds for triangle
                     int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
-                    int maxX = (int) Math.min(img.getWidth() - 1,
-                            Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+                    int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
                     int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
-                    int maxY = (int) Math.min(img.getHeight() - 1,
-                            Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getHeight() - 1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
 
-                    double triangleArea =
-                            (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+                    double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
 
+                    // Calculate barycentric coordinates to determine if point is inside triangle
                     for (int y = minY; y <= maxY; y++) {
                         for (int x = minX; x <= maxX; x++) {
-                            double b1 =
-                                    ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
-                            double b2 =
-                                    ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
-                            double b3 =
-                                    ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
-                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                            double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                            double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                            double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
 
+                            // If point is inside triangle, draw pixel
+                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
                                 double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
                                 int zIndex = y * img.getWidth() + x;
+                                // If pixel is closer to camera, draw pixel. Color pixel with shade
                                 if (zBuffer[zIndex] < depth) {
-                                    img.setRGB(x, y, t.color.getRGB());
+                                    Color shadedColor = getShade(t.color, angleCos);
+                                    img.setRGB(x, y, shadedColor.getRGB());
                                     zBuffer[zIndex] = depth;
                                 }
                             }
@@ -128,6 +144,18 @@ public class DemoViewer {
 
         frame.setSize(400, 400);
         frame.setVisible(true);
+    }
+
+    public static Color getShade(Color color, double shade) {
+        double redLinear = Math.pow(color.getRed(), 2.4) * shade;
+        double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
+        double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
+
+        int red = (int) Math.pow(redLinear, 1 / 2.4);
+        int green = (int) Math.pow(greenLinear, 1 / 2.4);
+        int blue = (int) Math.pow(blueLinear, 1 / 2.4);
+
+        return new Color(red, green, blue);
     }
 }
 
